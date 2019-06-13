19cf1d06f5ee4378cb977b6a046862f363b71cd7
Mathias Schilling <m@matchilling.com>
2019-06-13 22:35:11 +0200

    Add "get_random_personalized_joke" function

--- migration/0002/initial_setup_stored_procedures.sql
+++ migration/0002/initial_setup_stored_procedures.sql
@@ -288,4 +288,35 @@ WHERE joke_id = update_joke.joke_id RETURNING
 $$ LANGUAGE sql;
 COMMENT ON FUNCTION update_joke(SLUGID, JSONB, VARCHAR) IS 'Update a joke.';
 
+CREATE OR REPLACE FUNCTION get_random_personalized_joke(substitute VARCHAR,
+                                                        categories TEXT DEFAULT null) RETURNS joke AS
+$$
+
+SELECT j.categories,
+       j.created_at,
+       j.joke_id,
+       j.updated_at,
+       REGEXP_REPLACE(
+               REGEXP_REPLACE(
+                       REGEXP_REPLACE(j.value, 'Chuck Norris', get_random_personalized_joke.substitute, 'ig'),
+                       'Chuck', get_random_personalized_joke.substitute, 'ig'),
+               'Norris', get_random_personalized_joke.substitute, 'ig') as value
+FROM joke AS j
+WHERE CASE
+          WHEN get_random_personalized_joke.categories IS NOT NULL
+              THEN j.categories IS NOT NULL AND
+                   j.categories ?| regexp_split_to_array(get_random_personalized_joke.categories, ',')
+          ELSE true
+    END
+  AND (j.value LIKE ('%Chuck Norris %') OR j.value LIKE ('%Chuck %'))
+  AND j.value NOT ILIKE ('% he %')
+  AND j.value NOT ILIKE ('% him %')
+  AND j.value NOT ILIKE ('% his %')
+ORDER BY RANDOM()
+LIMIT 1;
+
+$$ LANGUAGE sql;
+
+COMMENT ON FUNCTION get_random_personalized_joke(VARCHAR, TEXT) IS 'Get a random personalized joke.';
+
 COMMIT TRANSACTION;
